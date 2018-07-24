dd07c5840a3cd9b14336c77e11697de37197e260
Jude Ocheje <ochejejude@gmail.com>
2018-07-24 14:23:55 +0100

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const chalk = require('chalk')
 
+const { ObjectID } = require('mongodb')
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
@@ -31,6 +32,24 @@ app.get('/todos', (req, res) => {
     res.json({todos})
   }, (err) => {
     res.status(400).send()
+  })
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send()
+    }
+
+    res.json({todo})
+  }, (err) => {
+    res.status(400).send(err)
   })
 })
 
